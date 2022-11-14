dcd782de267e15a81198295e594f1fe204a6f131
Abd Algani Sabbagh <abdalganisabbagh@gmail.com>
2022-11-14 22:25:01 +0300

    test

---
+++ lib/ggggg.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ssss extends StatefulWidget {
+  const ssss({Key? key}) : super(key: key);
+
+  @override
+  State<ssss> createState() => _ssssState();
+}
+
+class _ssssState extends State<ssss> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
